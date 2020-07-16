1c5b8cab3ac2e9ca29c77c618389e975be8e17c1
yujin <275658575@qq.com>
2020-07-16 18:09:22 +0800

    添加彩云token

--- app/src/main/java/com/yj/weather/App.kt
+++ app/src/main/java/com/yj/weather/App.kt
@@ -6,6 +6,9 @@ import android.content.Context
 
 class App:Application() {
     companion object{
+
+        const val CAIYUN_TOKEN = "AYl2K28IQmNra1E9"
+
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
     }
