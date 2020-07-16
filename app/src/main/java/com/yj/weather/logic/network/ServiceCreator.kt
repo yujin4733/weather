899dc2977e984775290ac16e5ea4195a8369fb3a
yujin <275658575@qq.com>
2020-07-16 18:10:44 +0800

    修改

--- app/src/main/java/com/yj/weather/logic/network/ServiceCreator.kt
+++ app/src/main/java/com/yj/weather/logic/network/ServiceCreator.kt
@@ -1,6 +1,17 @@
 package com.yj.weather.logic.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ServiceCreator {
 
     private const val BASE_RUL = "https://api.caiyunapp.com/"
+
+    private val retrofit = Retrofit.Builder().baseUrl(BASE_RUL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun <T> create(serviceClass: Class<T>): T = retrofit.create(serviceClass)
+
+    inline fun <reified T> create(): T = create(T::class.java)
 }
