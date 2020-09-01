dde5c89dd361f461c613aa472df840009b3e96f7
yujin <275658575@qq.com>
2020-09-01 09:42:09 +0800

    配置全面屏

--- app/src/main/java/com/yj/weather/base/BaseActivity.kt
+++ app/src/main/java/com/yj/weather/base/BaseActivity.kt
@@ -1,7 +1,6 @@
 package com.yj.weather.base
 
 import android.os.Bundle
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -9,6 +8,7 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(layoutRes())
 
     }
